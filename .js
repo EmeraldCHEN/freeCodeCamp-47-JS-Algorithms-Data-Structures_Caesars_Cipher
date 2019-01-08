54036b6bd339178ba84b110e86b1fc16bde65a94
Emerald Chen <ahyuancy@gmail.com>
2019-01-08 16:44:34 +1300

    Update .js

--- .js
+++ .js
@@ -2,8 +2,7 @@ function rot13(str){
     let rotCharArray = [];
     let regex = /[A-Z]/;
     let strArray = str.split('');
-    for(let x in str){
-
+    for(let x in strArray){
         if(regex.test(strArray[x])){
             rotCharArray.push((strArray[x].charCodeAt() - 65 + 13) % 26 + 65);
         }else{
@@ -12,12 +11,10 @@ function rot13(str){
     }
     // The apply() method accepts arguments in an array and the first argument (null) does not matter, it could be " ", 0, etc.
     return String.fromCharCode.apply(null, rotCharArray);
-
-
 }
+rot13("SERR PBQR PNZC"); // decode to FREE CODE CAMP
+rot13("SERR CVMMN!"); // FREE PIZZA!
 
-
-rot13("SERR PBQR PNZC");
 
 
 
